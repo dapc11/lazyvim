5f867a710cc8713406fb4132dd521efab79c10ff
Daniel Tingelöf <daniel.tingelof@gmail.com>
2023-07-11 20:48:49 +0200

    Fix diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,24 +1,35 @@
 return {
-  "sindrets/diffview.nvim",
-  event = "VeryLazy",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  cmd = {
-    "DiffviewOpen",
-    "DiffviewClose",
-    "DiffviewFileHistory",
-    "DiffviewRefresh",
-    "DiffviewToggleFiles",
-    "DiffviewFocusFiles",
+  {
+    "sindrets/diffview.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = {
+      "DiffviewOpen",
+      "DiffviewClose",
+      "DiffviewFileHistory",
+      "DiffviewRefresh",
+      "DiffviewToggleFiles",
+      "DiffviewFocusFiles",
+    },
+    keys = {
+      { "<leader>gq", vim.cmd.DiffviewClose, "Diffview Close" },
+      { "<leader>gd", vim.cmd.DiffviewOpen, "Diffview (all modified files)" },
+      {
+        "<leader>gl",
+        function()
+          vim.cmd.DiffviewFileHistory("%")
+        end,
+        "Diffview current file history",
+      },
+    },
   },
-  keys = {
-    { "<leader>gq", vim.cmd.DiffviewClose, "Diffview Close" },
-    { "<leader>gd", vim.cmd.DiffviewOpen, "Diffview (all modified files)" },
-    {
-      "<leader>gl",
-      function()
-        vim.cmd.DiffviewFileHistory("%")
-      end,
-      "Diffview current file history",
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {
+      defaults = {
+        ["<leader>g"] = { name = "+Git" },
+      },
     },
   },
 }
