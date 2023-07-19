a49a9e640ba8cbf954ce764b230a6be68307b79e
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-07-19 09:24:31 +0200

    Fix setup of nvim-dap

--- lua/plugins/lang/python.lua
+++ lua/plugins/lang/python.lua
@@ -91,8 +91,10 @@ return {
         { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class" },
       },
       config = function()
-        local path = require("mason-registry").get_package("debugpy"):get_install_path()
-        require("dap-python").setup(path .. "/venv/bin/python")
+        pcall(function()
+          local path = require("mason-registry").get_package("debugpy"):get_install_path()
+          require("dap-python").setup(path .. "/venv/bin/python")
+        end)
       end,
     },
   },
