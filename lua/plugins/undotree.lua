6a152026c9acb32aa02561b3bff6dada9de9c9ac
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-07-20 20:11:24 +0200

    Change keymap for undotree

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,6 +1,6 @@
 return {
   "mbbill/undotree",
   keys = {
-    { "<leader>u", vim.cmd.UndotreeToggle, desc = "Toggle Undotree" },
+    { "<leader>U", vim.cmd.UndotreeToggle, desc = "Toggle Undotree" },
   },
 }
