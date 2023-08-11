b04ee1307b79a6d2931db790354a63dd8d1d650e
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-08-11 17:34:22 +0200

    Go setup tweaks

--- lua/plugins/coding/go.lua
+++ lua/plugins/coding/go.lua
@@ -80,20 +80,27 @@ return {
       sign_priority = 5,
       test_runner = "go", -- one of {`go`, `richgo`, `dlv`, `ginkgo`, `gotestsum`}
       luasnip = true,
+      trouble = true,
     },
     ft = { "go", "gomod" },
     build = 'lua require("go.install").update_all_sync()',
   },
   {
     "jose-elias-alvarez/null-ls.nvim",
+    dependencies = { "ray-x/go.nvim" },
     opts = function(_, opts)
       if type(opts.sources) == "table" then
         local nls = require("null-ls")
+        local gotest = require("go.null_ls").gotest()
+        local gotest_codeaction = require("go.null_ls").gotest_action()
+        local golangci_lint = require("go.null_ls").golangci_lint()
+        table.insert(opts.sources, gotest)
+        table.insert(opts.sources, golangci_lint)
+        table.insert(opts.sources, gotest_codeaction)
         vim.list_extend(opts.sources, {
           nls.builtins.code_actions.gomodifytags,
           nls.builtins.code_actions.impl,
           nls.builtins.formatting.gofumpt,
-          nls.builtins.formatting.goimports_reviser,
         })
       end
     end,
---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "folke/trouble.nvim",
+    keys = {
+      { "<leader>xl", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
+      { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
+    },
+  },
+}
